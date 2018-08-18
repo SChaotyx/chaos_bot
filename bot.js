5962980c103c9b40b1a0b4a319cf71fa7ef5c1d2
SChaotyx <42502797+SChaotyx@users.noreply.github.com>
2018-08-18 20:29:28 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,12 @@
-console.log('Online Now!');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+bot.on('ready',() => {
+  console.log('Ready!');
+});
+bot.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+});
 bot.login (process.env.BOT_TOKEN);
