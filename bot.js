5a739621ed5b666ae773727cfc78318324c65ebf
SChaotyx <42502797+SChaotyx@users.noreply.github.com>
2018-08-18 20:20:54 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,4 +2,4 @@ console.log('Online Now!');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.login ('NDc1MTkxMTgxNTE3NzgzMDQx.Dlo4Uw.3VBKsByKSJcfzNSIY4eSjQ5MNok');
+bot.login (process.env.BOT_TOKEN);
