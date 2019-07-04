c1b6c58a4515d3fd5c955c7bc2372fb6c1b03497
SChaotyx <42502797+SChaotyx@users.noreply.github.com>
2019-07-04 13:20:40 -0300

    Add files via upload
    TEST COMMANDS

--- bot.js
+++ bot.js
@@ -1,12 +1,61 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-
-bot.on('ready',() => {
-  console.log('Ready!');
+var Discord = require('discord.io');
+var logger = require('winston');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+	colorize: true
 });
-bot.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
-  }
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+	token: "NTk2MTY2MTQ3NTI3OTMzOTUy.XR3IGg.lkYJ6lPcxf3x32hqaz4dSEeUj3I",
+	autorun: true
 });
-bot.login (process.env.BOT_TOKEN);
+bot.on('ready', function (evt) {
+	logger.info('Connected');
+	logger.info('Logged in as: ');
+	logger.info(bot.username + ' – (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+	// Nuestro bot necesita saber si ejecutará un 
+	// Escuchará los mensajes que empiecen con '¡'
+	if (message.substring(0, 5) == 'gdps!') {
+		var args = message.substring(1).split(' ');
+		var cmd = args[0];
+		if (message.substring(9, 5) == 'ping') {
+			bot.sendMessage({to: channelID, message: 'Pong!'});
+		}
+		if (message.substring(12, 5) == 'profile') {
+			args = message.substring(13);
+			var postData={userName: args, channel: channelID};
+			require('request').post({
+				uri:"http://arafrybb.000webhostapp.com/dashboard/botcmd/userProfile.php",
+				headers:{'content-type': 'application/x-www-form-urlencoded'},
+				body:require('querystring').stringify(postData)
+			},
+			function(err,res,body){
+				console.log(body);
+				console.log(res.statusCode);
+			});
+		}
+		if (message.substring(10, 5) == 'level') {
+			args = message.substring(11);
+			var postData={levelID_Name: args, channel: channelID};
+			require('request').post({
+				uri:"http://arafrybb.000webhostapp.com/dashboard/botcmd/levelStats.php",
+				headers:{'content-type': 'application/x-www-form-urlencoded'},
+				body:require('querystring').stringify(postData)
+			},
+			function(err,res,body){
+				console.log(body);
+				console.log(res.statusCode);
+			});
+		}
+	}
+});
+/*
+DEPENDENCIAS 
+npm i request
+npm install discord.io winston
+npm install https://github.com/woor/discord.io/tarball/gateway_v6
+*/
