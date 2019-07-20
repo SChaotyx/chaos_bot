ff0d59b6551ea450fe5dac9f39985466cef0499a
SChaotyx <42502797+SChaotyx@users.noreply.github.com>
2019-07-20 10:45:18 -0300

    add help cmd

--- bot.js
+++ bot.js
@@ -12,6 +12,11 @@ var bot = new Discord.Client({
 	autorun: true
 });
 bot.on('ready', function (evt) {
+	bot.setPresence({
+        game: {
+            name: 'v1.1.0 | Use gdps!help'
+        }
+	});
 	logger.info('Connected');
 	logger.info('Logged in as: ');
 	logger.info(bot.username + ' – (' + bot.id + ')');
@@ -21,6 +26,44 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 	if (message.substring(0, 5) == 'gdps!') {
 		var args = message.substring(1).split(' ');
 		var cmd = args[0];
+		if (message.substring(9, 5) == 'help') {
+			bot.sendMessage({
+				to: channelID,
+				message: "<@"+userID+">, here all <@596166147527933952> commands.",
+				embed: {
+					title: "<:info:588245545643016224> ChaosBot Commands",
+					thumbnail: {
+						url: process.env.HOSTING+"resources/misc/gdpsbot.png"
+					},
+					footer: {
+						icon_url: process.env.HOSTING+"resources/misc/gdpsbot.png",
+						text: "Chaos-Bot v1.2.0"
+					},
+					fields: [
+					{
+						"name": "Bot prefix: `gdps!`",
+						"value": "Use `gdps!help <command>` to view the detailed documentation of a specific command."
+					},
+					{
+						"name": "`gdps!profile <UserName>`",
+						"value": "example: `gdps!profile Alexander73`."
+					},
+					{
+						"name": "`gdps!level <LevelName or LevelID>`",
+						"value": "example: `gdps!level 19457` or `gdps!level DeadLocked`."
+					},
+					{
+						"name": "`gdps!daily` | `gdps!weekly`",
+						"value": "Displays the current Daily / weekly level in the GDPS."
+					},
+					{
+						"name": "`gdps!stats`",
+						"value": "Show the general statistics of the GDPS."
+					},					
+					]
+				}
+			});			
+		}
 		if (message.substring(9, 5) == 'ping') {
 			bot.sendMessage({to: channelID, message: 'Pong!'});
 		}
