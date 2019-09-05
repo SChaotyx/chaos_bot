e0c322bf3230545dfe37353fd912d707ee4d0fb1
SChaotyx <42502797+SChaotyx@users.noreply.github.com>
2019-09-05 11:19:32 -0300

    Rewritten to Discord.js

--- bot.js
+++ bot.js
@@ -1,144 +1,108 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-	colorize: true
+const Discord = require("discord.js");
+const  client = new Discord.Client();
+const gs = require("./mainLib.js");
+
+//HEROKU CONFIG VARS
+var bottoken = process.env.BOT_TOKEN;
+var prefix = 'gdps!';
+var gdpshost = process.env.HOSTING;
+var channel_logs = process.env.CHANNEL_LOG;
+
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e));
+client.on("debug", (e) => console.info(e));
+
+client.on("ready", () => {
+  client.user.setActivity('v1.2.0 | Use gdps!help', { type: 'PLAYING' });
+   console.log("CONNECTED.");
+   client.channels.get(channel_logs).send('```Connected.```')
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-	token: process.env.BOT_TOKEN,
-	autorun: true
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix)) return;
+  if (message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  var userID = message.member.id;
+  var channelID = message.channel.id;
+
+  //COMMANDS
+  if(command === 'ping'){
+    message.channel.send("pong!");
+  }
+  if(command === 'help'){
+    message.channel.send(gs.premsg(userID, 1), gs.prebuild(userID, channelID, gdpshost, 'help'));
+  }
+  if(command === 'about'){
+    message.channel.send(gs.premsg(userID, 2), gs.prebuild(userID, channelID, gdpshost, 'about'));
+  }
+  if(command === 'profile'){
+    var usuario = args.slice(0).join(' ');
+    var postData = {userName: usuario, channel: channelID, tagID: userID};
+    require('request').post({
+      uri:gdpshost+"incl/discord/cmd/userProfile.php",
+      headers:{'content-type': 'application/x-www-form-urlencoded'},
+      body:require('querystring').stringify(postData)
+    },
+    function(err,res,body){
+      console.log(body);
+      console.log(res.statusCode);
+      client.channels.get(channel_logs).send('```'+body+'```');
+    });
+  }
+  if(command === 'level'){
+    var level = args.slice(0).join(' ');
+    var postData={levelID_Name: level, channel: channelID, tagID: userID};
+			require('request').post({
+				uri:gdpshost+"incl/discord/cmd/levelStats.php",
+				headers:{'content-type': 'application/x-www-form-urlencoded'},
+				body:require('querystring').stringify(postData)
+			},
+			function(err,res,body){
+				console.log(body);
+				console.log(res.statusCode);
+				client.channels.get(channel_logs).send('```'+body+'```');
+			});
+  }
+  if(command === 'stats'){
+    var postData={channel: channelID, tagID: userID};
+		require('request').post({
+			uri:gdpshost+"incl/discord/cmd/serverStats.php",
+			headers:{'content-type': 'application/x-www-form-urlencoded'},
+			body:require('querystring').stringify(postData)
+		},
+		function(err,res,body){
+			console.log(body);
+			console.log(res.statusCode);
+			client.channels.get(channel_logs).send('```'+body+'```');
+		});
+  }
+  if(command === 'daily'){
+    var postData={channel: channelID, type: 0, tagID: userID};
+    require('request').post({
+      uri:gdpshost+"incl/discord/cmd/currentDaily.php",
+      headers:{'content-type': 'application/x-www-form-urlencoded'},
+      body:require('querystring').stringify(postData)
+    },
+    function(err,res,body){
+      console.log(body);
+      console.log(res.statusCode);
+      client.channels.get(channel_logs).send('```'+body+'```');
+    });
+  }
+  if(command === 'weekly'){
+    var postData={channel: channelID, type: 1, tagID: userID};
+    require('request').post({
+      uri:gdpshost+"incl/discord/cmd/currentDaily.php",
+      headers:{'content-type': 'application/x-www-form-urlencoded'},
+      body:require('querystring').stringify(postData)
+    },
+    function(err,res,body){
+      console.log(body);
+      console.log(res.statusCode);
+      client.channels.get(channel_logs).send('```'+body+'```');
+    });
+  }
 });
-bot.on('ready', function (evt) {
-	bot.setPresence({
-        game: {
-            name: 'v1.1.0 | Use gdps!help'
-        }
-	});
-	logger.info('Connected');
-	logger.info('Logged in as: ');
-	logger.info(bot.username + ' – (' + bot.id + ')');
-	bot.sendMessage({to: process.env.CHANNEL_LOG, message: '**'+bot.username+' Connected**'});
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-	if (message.substring(0, 5) == 'gdps!') {
-		var args = message.substring(1).split(' ');
-		var cmd = args[0];
-		if (message.substring(9, 5) == 'help') {
-			bot.sendMessage({
-				to: channelID,
-				message: "<@"+userID+">, here all <@596166147527933952> commands.",
-				embed: {
-					title: "<:info:588245545643016224> ChaosBot Commands",
-					thumbnail: {
-						url: process.env.HOSTING+"resources/misc/gdpsbot.png"
-					},
-					footer: {
-						icon_url: process.env.HOSTING+"resources/misc/gdpsbot.png",
-						text: "Chaos-Bot v1.1.0"
-					},
-					fields: [
-					{
-						"name": "Bot prefix: `gdps!`",
-						"value": "Use `gdps!help <command>` to view the detailed documentation of a specific command."
-					},
-					{
-						"name": "`gdps!profile <UserName>`",
-						"value": "example: `gdps!profile Alexander73`."
-					},
-					{
-						"name": "`gdps!level <LevelName or LevelID>`",
-						"value": "example: `gdps!level 19457` or `gdps!level DeadLocked`."
-					},
-					{
-						"name": "`gdps!daily` | `gdps!weekly`",
-						"value": "Displays the current Daily / weekly level in the GDPS."
-					},
-					{
-						"name": "`gdps!stats`",
-						"value": "Show the general statistics of the GDPS."
-					},					
-					]
-				}
-			});			
-		}
-		if (message.substring(9, 5) == 'ping') {
-			bot.sendMessage({to: channelID, message: 'Pong!'});
-		}
-		if (message.substring(12, 5) == 'profile') {
-			args = message.substring(13);
-			var postData={userName: args, channel: channelID, tagID: userID};
-			require('request').post({
-				uri:process.env.HOSTING+"incl/discord/cmd/userProfile.php",
-				headers:{'content-type': 'application/x-www-form-urlencoded'},
-				body:require('querystring').stringify(postData)
-			},
-			function(err,res,body){
-				console.log(body);
-				console.log(res.statusCode);
-				bot.sendMessage({to: process.env.CHANNEL_LOG, message: '```'+body+'```'});
-			});
-		}
-		if (message.substring(10, 5) == 'level') {
-			args = message.substring(11);
-			var postData={levelID_Name: args, channel: channelID, tagID: userID};
-			require('request').post({
-				uri:process.env.HOSTING+"incl/discord/cmd/levelStats.php",
-				headers:{'content-type': 'application/x-www-form-urlencoded'},
-				body:require('querystring').stringify(postData)
-			},
-			function(err,res,body){
-				console.log(body);
-				console.log(res.statusCode);
-				bot.sendMessage({to: process.env.CHANNEL_LOG, message: '```'+body+'```'});
-			});
-		}
-		if (message.substring(10, 5) == 'stats') {
-			var postData={channel: channelID, tagID: userID};
-			require('request').post({
-				uri:process.env.HOSTING+"incl/discord/cmd/serverStats.php",
-				headers:{'content-type': 'application/x-www-form-urlencoded'},
-				body:require('querystring').stringify(postData)
-			},
-			function(err,res,body){
-				console.log(body);
-				console.log(res.statusCode);
-				bot.sendMessage({to: process.env.CHANNEL_LOG, message: '```'+body+'```'});
-			});
-		}
-		if (message.substring(10, 5) == 'daily') {
-			var postData={channel: channelID, type: 0, tagID: userID};
-			require('request').post({
-				uri:process.env.HOSTING+"incl/discord/cmd/currentDaily.php",
-				headers:{'content-type': 'application/x-www-form-urlencoded'},
-				body:require('querystring').stringify(postData)
-			},
-			function(err,res,body){
-				console.log(body);
-				console.log(res.statusCode);
-				bot.sendMessage({to: process.env.CHANNEL_LOG, message: '```'+body+'```'});
-			});
-		}
-		if (message.substring(11, 5) == 'weekly') {
-			var postData={channel: channelID, type: 1, tagID: userID};
-			require('request').post({
-				uri:process.env.HOSTING+"incl/discord/cmd/currentDaily.php",
-				headers:{'content-type': 'application/x-www-form-urlencoded'},
-				body:require('querystring').stringify(postData)
-			},
-			function(err,res,body){
-				console.log(body);
-				console.log(res.statusCode);
-				bot.sendMessage({to: process.env.CHANNEL_LOG, message: '```'+body+'```'});
-			});
-		}
-	}
-});
-/*
-DEPENDENCIAS 
-npm i request
-npm install discord.io winston
-npm install https://github.com/woor/discord.io/tarball/gateway_v6
-*/
+client.login(bottoken);
---
+++ mainLib.js
@@ -0,0 +1,74 @@
+//MAIN FUNCTIONS IS HERE
+exports.premsg = function(userID, id){
+    switch(id){
+        case 1: msg = "<@"+userID+">, here all <@596166147527933952> commands.";
+        break;
+        case 2: msg = "https://discord.gg/7gJeArr";
+        break;
+    }
+    return msg
+}
+exports.prebuild = function(userID, channelID, gdpshost, param){
+    if(param === 'help'){
+        const embed = { 
+            embed: {
+                title: "<:info:588245545643016224> ChaosBot Commands",
+                thumbnail:{ url: gdpshost+"resources/misc/gdpsbot.png" },
+                timestamp: new Date(),
+                footer: {
+                    icon_url: gdpshost+"resources/misc/gdpsbot.png",
+                    text: "Chaos-Bot v1.2.0"
+                },
+                fields: [
+                    {
+                        "name": "Bot prefix: `gdps!`",
+                        "value": "Use `gdps!help <command>` to view the detailed documentation of a specific command."},
+                    {
+                            "name": "`gdps!profile <UserName>`",
+                            "value": "Example: `gdps!profile Alexander73`."},
+                    {
+                        "name": "`gdps!level <LevelName or LevelID>`",
+                        "value": "Example: `gdps!level 19457` or `gdps!level DeadLocked`."},
+                    {
+                        "name": "`gdps!daily` | `gdps!weekly`",
+                        "value": "Displays the current Daily / weekly level in the GDPS."},
+                    {
+                        "name": "`gdps!stats`",
+                        "value": "Show the general statistics of the GDPS."},					
+                    {
+                        "name": "`gdps!about`",
+                        "value": "Show bot info"},					
+                ]
+            }
+        }
+        return embed;
+    }
+    if(param === 'about'){
+        const embed = {
+            embed: {
+                title: "<:auto:588254712017584139> ChaosBot",
+                thumbnail:{ url: gdpshost+"resources/misc/gdpsbot.png" },
+                timestamp: new Date(),
+                footer: {
+                    icon_url: gdpshost+"resources/misc/SC.png",
+                    text: "SChaotyx#5120"
+                },
+                fields: [
+                    {
+                    "name": "<:creator_points:588245543600390184> ChaosBot, Developed by SChaotyx <:dev:588246858460037121>",
+                    "value": "It is a simple bot for Geometry Dash Chaos Private Server,\n"+ 
+                    "with basic features to find levels and profiles within it.\n"+
+                    "If you have any problem contact me on my social networks",
+                    },
+                    {
+                    "name": "<:friends:588245550302756870> Social:",
+                    "value": "<:discord:588248737684520971>[Discord](https://discord.gg/7gJeArr)\n"+
+                    "<:tw:588248740200972288>[Twitter](https://twitter.com/SChaotyx)\n"+
+                    "<:yt:588249043596083200>[Youtbe](https://www.youtube.com/channel/UC4eZQQYmaD-Q90LD_tF6FrA)"
+                }
+            ]
+            }
+        }
+        return embed;
+    }
+}
