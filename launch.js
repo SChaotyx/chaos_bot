d49ad20632b0bbc513d848ac348c36d618b624b0
SChaotyx <Agustinangelcab@gmail.com>
2019-09-27 03:29:41 -0300

    keepOnline.js

--- launch.js
+++ launch.js
@@ -1,24 +1,7 @@
-//SETUP STARTUP
-const http = require('http');
-const express = require('express');
-const app = express();
-//
-app.use(express.static('public'));
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`); 
-}, 280000);
-
 const Discord = require("discord.js");
 const  client = new Discord.Client();
 const fs = require("fs");
-//require(__dirname + '/app/keepAlive.js')
+require(__dirname + '/app/keepAlive.js')
 require('dotenv').config()
 
 
@@ -54,7 +37,3 @@ client.on("ready", () => {
    client.channels.get(process.env.CHANNEL_LOG).send('```Connected.```')
 });
 client.login(process.env.BOT_TOKEN);
-/*
-client.on("error", (e) => console.error(e));
-client.on("warn", (e) => console.warn(e)); 
-*/
