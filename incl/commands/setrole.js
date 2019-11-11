a812c5e1906e3d9ba47eea8ab62f3ccc1eef623d
SChaotyx <Agustinangelcab@gmail.com>
2019-11-11 04:22:56 -0300

    auto delete msg

--- incl/commands/setrole.js
+++ incl/commands/setrole.js
@@ -3,11 +3,11 @@ exports.run = async(client, message, args) => {
     var roleID = args.slice(1).join(' ');
     let userdis = message.guild.members.get(userID);
     let roledis = message.guild.roles.get(roleID);
-    if(userdis.roles.has(roledis.id)) return;
+    if(userdis.roles.has(roledis.id)) return message.delete(1000);
     if(!roledis) return;
     if(!userdis) return;
     if (message.author.bot){
-        //if(!message.guild.member(bot.user).hasPermission('MANAGE_ROLES')) return;
+        if(!message.guild.member(client.user).hasPermission('MANAGE_ROLES')) return message.reply('Manage Roles permission is denied.');
         userdis.addRole(roledis);
         message.channel.send("<@"+userID+"> gets the <@&"+roleID+"> role");
     }else{
