d4bc9c1a649d31193ac6b8994b8153e3f83025d3
SChaotyx <Agustinangelcab@gmail.com>
2020-01-13 00:21:56 -0300

    request: beta#2

--- incl/cmd/reqcmd/.js
+++ incl/cmd/reqcmd/.js
@@ -6,38 +6,61 @@ exports.run = async(client, message, args) => {
     var levelID = args.slice(0)[0];
     var notes = args.slice(1).join(' ');
     if(isNaN(levelID)){
-        message.channel.send("Usa: `"+process.env.REQPREFIX+" <Level ID> <Notas>` sin `<>`\nEjemplo: `"+process.env.REQPREFIX+" 45514142 Collab con amigos.`\n las notas es opcional puedes no incluirla directamente.");
+        message.channel.send("Usa: `"+process.env.REQPREFIX+" <Level ID> <Notas>` sin `<>`\nEjemplo: `"+process.env.REQPREFIX+" 45514142 Collab con amigos.`\nlas notas es opcional puedes no incluirla.");
         message.delete(1000);
         return false;
     }
     GD.levels(levelID).then( levelData =>{
-        if(levelData === "-1"){
-            message.channel.send("Error: El nivel no existe.");
-            message.delete(1000);
-            return false;
-        }
-        var lvl = levelData[0];
-        if(lvl.length < 2){ 
-            message.channel.send("Error: el nivel es demasiado corto");
-            message.delete(1000);
-            return false;
-        }
-        if(lvl.stars > 0){ 
-            message.channel.send("Error: el nivel ya tiene estrellas.");
-            message.delete(1000);
-            return false;
-        }
-        let embedData = emb.levels({
-            lvl : lvl, 
-            type : "request", 
-            notes : notes, 
-            requser : message.author.tag, 
-            requserID : message.author.id, 
-            reqavatar : message.author.avatarURL, 
-            requestID: Date.now() / 1000 | 0
+        message.channel.fetchMessages().then(messages => {
+            var already = 0;
+            var count = 0;
+            var msgc = Number(messages.size);
+            messages.forEach( function(valor, indice, array) {
+                count++;
+                if(valor.author.id === client.user.id){
+                    if(valor.embeds[0].footer.text.split(" | ")[1].split(": ")[1]){
+                        var lvlID = valor.embeds[0].footer.text.split(" | ")[0].split(": ")[1];
+                        if(lvlID === levelID){
+                            already++;
+                        }
+                    }
+                }
+                if(msgc === count){
+                    if(already > 0){
+                        message.channel.send("Error: El nivel ya esta pendiente.");
+                        message.delete(1000);
+                        return false;
+                    }
+                    if(levelData === "-1"){
+                        message.channel.send("Error: El nivel no existe.");
+                        message.delete(1000);
+                        return false;
+                    }
+                    var lvl = levelData[0];
+                    if(lvl.length < 2){ 
+                        message.channel.send("Error: el nivel es demasiado corto");
+                        message.delete(1000);
+                        return false;
+                    }
+                    if(lvl.stars > 0){ 
+                        message.channel.send("Error: el nivel ya tiene estrellas.");
+                        message.delete(1000);
+                        return false;
+                    }
+                    let embedData = emb.levels({
+                        lvl : lvl, 
+                        type : "request", 
+                        notes : notes, 
+                        requser : message.author.tag, 
+                        requserID : message.author.id, 
+                        reqavatar : message.author.avatarURL, 
+                        requestID: Date.now() / 1000 | 0
+                    });
+                    //console.log(embedData);
+                    message.channel.send(embedData);
+                    message.delete(1000);
+                }
+            });
         });
-        //console.log(embedData);
-        message.channel.send(embedData);
-        message.delete(1000);
     });
 }
--- incl/lib/embeds.js
+++ incl/lib/embeds.js
@@ -74,13 +74,12 @@ class embeds extends EventEmitter {
             let reqlevelstats = "<:down2:588245545303408671> `"+lvl.downloads+"` "+likeicon+" `"+lvl.likes+"` <:length:588245546108583946> `"+lengtharr[parseInt(lvl.length)]+"`";
             if(Data.status === "OK"){
                 var status = "<:success:588245546469294111> Sent.";
-                embedData.setColor('#fffffe');
+                embedData.setColor('#2fff00');
             }else{
                 var status = "<:cross:588245544200175621> Not sent.";
-                embedData.setColor('#fffffe');
+                embedData.setColor('#ff0000');
             }
             if(Data.notes === ""){ var notes = ""; }else{ var notes = Data.notes; }
-            
             embedData.setTitle(reqTitle);
             embedData.addField(name1, reqlevelstats);
             embedData.addField(name2, name3);
