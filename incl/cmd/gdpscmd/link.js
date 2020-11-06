25d89774b6410c45d646de16ac5c5906eed215dc
SChaotyx <agustinangelcab@gmail.com>
2020-11-06 12:34:01 -0300

    vincular cuentas funciona otra vez (member.id)

--- incl/cmd/gdpscmd/link.js
+++ incl/cmd/gdpscmd/link.js
@@ -1,6 +1,7 @@
 exports.run = async(client, message, args) => {
     var userdata = args.slice(0).join(' ');
-    var postData={userData: userdata, channel: message.channel.id, tagID: message.author.id, userTag: message.member.user.tag};
+    var postData={userData: userdata, channel: message.channel.id, tagID: message.author.id, userTag: message.author.username+"#"+message.author.discriminator};
+    console.log(message.author.username+"#"+message.author.discriminator);
     require('request').post({
         uri: process.env.HOSTING + "incl/discord/cmd/linkAccount.php",
         headers:{'content-type': 'application/x-www-form-urlencoded'},
