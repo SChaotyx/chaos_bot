672589740224f86be94e0bdde77c264a4f20627b
SChaotyx <42502797+SChaotyx@users.noreply.github.com>
2021-07-10 02:19:45 -0300

    revert all

--- incl/cmd/gdpscmd/setrole.js
+++ incl/cmd/gdpscmd/setrole.js
@@ -1,15 +1,14 @@
 exports.run = async(client, message, args) => {
     var userID = args.slice(0, 1).join(' ');
     var roleID = args.slice(1).join(' ');
-    let userdis = message.guild.members.fetch(userID);
-    let roledis = message.guild.roles.fetch(roleID);
+    let userdis = message.guild.members.get(userID);
+    let roledis = message.guild.roles.get(roleID);
     if(!roledis) return client.channels.get(process.env.CHANNEL_LOG).send("```El rol "+roleID+" no existe.```");
     if(!userdis) return client.channels.get(process.env.CHANNEL_LOG).send("```El usuario "+userID+" no existe.```");
     if(userdis.roles.has(roledis.id)) return message.delete(1000);
     if (message.author.bot){
         if(!message.guild.member(client.user).hasPermission('MANAGE_ROLES')) return message.reply('Manage Roles permission is denied.');
-        //userdis.addRole(roledis);
-        message.guild.member(userID).roles.add(roleID);
+        userdis.addRole(roledis);
         client.channels.get(process.env.CHANNEL_NOTIFY).send("<@"+userID+">, ¡Obtuviste un rol especial! <@&"+roleID+">.");
     }else{
         message.channel.send("<@"+userID+"> No no no");
