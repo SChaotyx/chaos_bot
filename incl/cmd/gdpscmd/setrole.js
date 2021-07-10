c7ded470618d40184bc247c8b0ce56dbd17d04cb
SChaotyx <42502797+SChaotyx@users.noreply.github.com>
2021-07-10 01:57:52 -0300

    test 2

--- incl/cmd/gdpscmd/setrole.js
+++ incl/cmd/gdpscmd/setrole.js
@@ -1,10 +1,10 @@
 exports.run = async(client, message, args) => {
     var userID = args.slice(0, 1).join(' ');
     var roleID = args.slice(1).join(' ');
-    let userdis = message.guild.members.get(userID);
+    let userdis = message.guild.members.fetch(userID);
     let roledis = message.guild.roles.get(roleID);
     if(!roledis) return client.channels.get(process.env.CHANNEL_LOG).send("```El rol "+roleID+" no existe.```");
-    //if(!userdis) return client.channels.get(process.env.CHANNEL_LOG).send("```El usuario "+userID+" no existe.```");
+    if(!userdis) return client.channels.get(process.env.CHANNEL_LOG).send("```El usuario "+userID+" no existe.```");
     if(userdis.roles.has(roledis.id)) return message.delete(1000);
     if (message.author.bot){
         if(!message.guild.member(client.user).hasPermission('MANAGE_ROLES')) return message.reply('Manage Roles permission is denied.');
